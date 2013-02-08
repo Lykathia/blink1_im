1a9f666eef76ca2a88bfd18905c1cb65fe37c353
Evan Lowry <freyr@edev1.(none)>
2013-02-07 21:59:35 -0400

    Figured out dbus interface

--- pidgin-to-blink1.sh
+++ pidgin-to-blink1.sh
@@ -22,15 +22,10 @@ DBUS_INTERFACE="im.pidgin.purple.PurpleInterface"
 DBUS_SERVICE="im.pidgin.purple.PurpleService"
 DBUS_PATH="/im/pidgin/purple/PurpleObject"
 
-# Pigdin Status'
-STATUS_OFFLINE=1
-STATUS_AVAILABLE=2
-STATUS_UNAVAILABLE=3
-STATUS_INVISIBLE=4
-STATUS_AWAY=5
-STATUS_EXTENDED_AWAY=6
-STATUS_MOBILE=7
-STATUS_TUNE=8
+# Set colours
+STATUS_AVAILABE=0,255,0 # Green
+STATUS_AWAY=255,255,0   # Yellow
+STATUS_BUSY=255,0,0     # Red
 
 # On exit, shut off the blink1
 trap "{ $BLINK1 --off > /dev/null 2>&1; exit $?; }" SIGINT SIGTERM
@@ -38,6 +33,17 @@ trap "{ $BLINK1 --off > /dev/null 2>&1; exit $?; }" SIGINT SIGTERM
 # Daemon
 dbus-monitor --profile "type='signal',interface='$DBUS_INTERFACE',member='SavedstatusChanged'" | 
 while read -r line; do
-    STATUS=`dbus-send --print-reply --dest=$DBUS_SERVICE $DBUS_PATH $DBUS_INTERFACE.PurpleSavedstatusGetCurrent`
+    STATUS_ID=`dbus-send --print-reply=literal --dest=$DBUS_SERVICE $DBUS_PATH $DBUS_INTERFACE.PurpleSavedstatusGetCurrent | cut -d ' ' -f5`
+    STATUS=`dbus-send --print-reply=literal --dest=$DBUS_SERVICE $DBUS_PATH $DBUS_INTERFACE.PurpleSavedstatusGetTitle int32:$STATUS_ID`
+
     echo $STATUS
+    #Handle colour settings ...
+    case "$STATUS" in 
+        "Available")
+            $BLINK1 --rgb $STATUS_AVAILABLE > /dev/null 2>&1;;
+        "Away") 
+            $BLINK1 --rgb $STATUS_AWAY > /dev/null 2>&1;;
+        "Busy") 
+            $BLINK1 --rgb $STATUS_BUSY > /dev/null 2>&1;;
+    esac
 done
